9b522b1bd1a42b47d3da55dacb3a70b6fde2c8e0
FedericoSarasso <federico.sarasso@itis.biella.it>
2023-04-04 17:27:33 +0200

    Punto uno Eseguibile, non testato tramite Test Unit

--- LavoroEducazioneCivica/Program.cs
+++ LavoroEducazioneCivica/Program.cs
@@ -1,25 +1,43 @@
 ﻿using System;
 
-string stringa = "ciao";
-int numero = 10;
-Console.Write(funzioneUno(stringa, numero));
-
-
-static string funzioneUno(string stringa, int numero)
+namespace LavoroEducazioneCivica
 {
-    string stringaCriptata = "";
-
-    foreach (char c in stringa)
+    class Program
     {
-        if (char.IsLetter(c))
+        static void Main(string[] args)
         {
-            char carattereCriptato = (char)(((int)c + numero - 65) % 26 + 65);
-            stringaCriptata += carattereCriptato;
+            string stringa = "";
+            int numero = 0;
+
+            //stringa = "ciao";
+            //numero = 10;
+
+            Console.Write(funzioneUno(stringa.ToUpper(), numero));
+
+
+            Console.ReadLine();
         }
-        else
+
+        static string funzioneUno(string stringa, int numero)
         {
-            stringaCriptata += c;
+            string stringaCriptata = "";
+
+            foreach (char c in stringa)
+            {
+                if (char.IsLetter(c))
+                {
+                    char carattereCriptato = (char)(((int)c + numero - 65) % 26 + 65);
+                    stringaCriptata += carattereCriptato;
+                }
+                else
+                {
+                    stringaCriptata += c;
+                }
+            }
+            return stringaCriptata;
         }
     }
-    return stringaCriptata;
 }
+
+
+
