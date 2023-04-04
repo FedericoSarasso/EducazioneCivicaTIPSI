50262e4063a15dd57326326596458f4f08375aac
FedericoSarasso <federico.sarasso@itis.biella.it>
2023-04-04 15:29:03 +0200

    Publiccazione Punto uno non testato

--- LavoroEducazioneCivica/Program.cs
+++ LavoroEducazioneCivica/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+string stringa = "ciao";
+int numero = 10;
+Console.Write(funzioneUno(stringa, numero));
+
+
+static string funzioneUno(string stringa, int numero)
+{
+    string stringaCriptata = "";
+
+    foreach (char c in stringa)
+    {
+        if (char.IsLetter(c))
+        {
+            char carattereCriptato = (char)(((int)c + numero - 65) % 26 + 65);
+            stringaCriptata += carattereCriptato;
+        }
+        else
+        {
+            stringaCriptata += c;
+        }
+    }
+    return stringaCriptata;
+}
