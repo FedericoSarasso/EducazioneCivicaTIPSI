19d52ff876471421eb8654f15757408aeba26094
FedericoSarasso <federico.sarasso@itis.biella.it>
2023-04-05 18:18:03 +0200

    Pubblicazione Punto Cinque funzionante e testato

---
+++ Test/FunzioneCinqueTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LavoroEducazioneCivica
+{
+    [TestClass]
+    public class FunzioneCinqueTests
+    {
+
+        [TestMethod]
+        public void Zero()
+        {
+            Assert.AreEqual(0, Program.FunzioneCinque("ABC", 0));
+        }
+
+        [TestMethod]
+        public void One()
+        {
+            Assert.AreEqual(6, Program.FunzioneCinque("ABC", 1));
+        }
+
+        [TestMethod]
+        public void Two()
+        {
+            Assert.AreEqual(12, Program.FunzioneCinque("ABC", 2));
+        }
+
+        [TestMethod]
+        public void Three()
+        {
+            Assert.AreEqual(2, Program.FunzioneCinque("ABC", 3));
+        }
+
+        [TestMethod]
+        public void Four()
+        {
+            Assert.AreEqual(24, Program.FunzioneCinque("ABC", 4));
+        }
+    }
+}
