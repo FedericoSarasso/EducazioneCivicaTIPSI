9fa43bf3625cc13fc4d104cab2cf90963b5da962
FedericoSarasso <federico.sarasso@itis.biella.it>
2023-04-05 12:38:14 +0200

    Pubblicazione Punto Tre funzionante e testo

---
+++ Test/FunzioneTreTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LavoroEducazioneCivica
+{
+    [TestClass]
+    public class FunzioneTreTests
+    {
+
+        [TestMethod]
+        public void Zero()
+        {
+            Assert.AreEqual(0, Program.FunzioneTre("ABC", 0));
+        }
+
+        [TestMethod]
+        public void One()
+        {
+            Assert.AreEqual(6, Program.FunzioneTre("ABC", 1));
+        }
+
+        [TestMethod]
+        public void Two()
+        {
+            Assert.AreEqual(12, Program.FunzioneTre("ABC", 2));
+        }
+
+        [TestMethod]
+        public void Three()
+        {
+            Assert.AreEqual(18, Program.FunzioneTre("ABC", 3));
+        }
+    }
+}
